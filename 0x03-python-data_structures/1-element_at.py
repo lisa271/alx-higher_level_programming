b2131a35bc839c487d5a624bfaaa1366eaad5fe8
lisa271 <lizbmaloba@gmail.com>
2023-01-09 15:05:30 +0300

    retrieving elements

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-#retrieves an element from a list like in C.
-
+#retrieves element
 def element_at(my_list, idx):
-        if idx < 0 or idx >= len(my_list):
-                    return
-                    return my_list[idx]
+    return
+return my_list[:]
