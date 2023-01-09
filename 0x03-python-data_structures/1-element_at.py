ef5c3205ee51af1abfd9c3214d521649f33262d0
lisa271 <lizbmaloba@gmail.com>
2023-01-09 19:36:27 +0300

    retrieve elements

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-#retrieves element
+#retrieving of elements
 def element_at(my_list, idx):
-
-    if idx < 0 or idx >= len(my_list):
-        return
-    return my_list[:]
+    if idx < 0 or idx > len(my_list) - 1:
+        return 'None'
+    else:
+        return my_list[idx]
