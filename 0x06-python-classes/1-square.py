3920fe764aa3e4bf41545c72eccfff108eb3db6f
lisa271 <lizbmaloba@gmail.com>
2023-01-25 10:48:53 -0800

    square with  private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""square class"""
+
+class Square:
+    def __init__(self, size):
+        """
+        atttributes:
+        size: size of the square
+        """
+        self.__size = size
