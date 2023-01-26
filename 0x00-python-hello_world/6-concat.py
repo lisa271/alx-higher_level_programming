7b81bd2b202dc3ba0ea5f4854f03e581c2ff240f
lisa271 <lizbmaloba@gmail.com>
2023-01-26 09:39:08 +0300

    concatenation

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-#concaneting two strings
-str1 = str1 + "" + str2
-print("f"Welcome to {str1}!)
+str1 = str1 + " " + str2
+print(f"Welcome to {str1}!")
