90ea3e95860a656692c35888540f8acc312d123f
lisa271 <lizbmaloba@gmail.com>
2023-02-07 01:51:10 -0800

    function that reads a text file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    """reads filename with utf-8"""
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end="")
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,4 @@
+task 0
+Write a function that reads a text file (UTF8) and prints it to stdout:
+      Prototype: def read_file(filename=""):
+      You must use the with statement
