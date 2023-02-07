91d2728d15c7b327f8de3ac1fe958676c71f5d8a
lisa271 <lizbmaloba@gmail.com>
2023-02-07 05:04:35 -0800

    write file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""defining write_file finction"""
+"""defining write_file with two arguments"""
 
 def write_file(filename="", text=""):
     with open(filename, "w", encoding = 'utf-8') as f:
-        return f.write()
+        return f.write("Writes file with two arguments\n")
