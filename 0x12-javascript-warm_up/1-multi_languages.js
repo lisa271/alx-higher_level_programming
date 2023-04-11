2cafd7b2c73226def36ce411fa9f0cdb9f36b693
lisa271 <lizbmaloba@gmail.com>
2023-04-11 14:19:47 -0700

    corrections

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 //a script printing three lines.
-console.log("Cis fun");
-console.log("Python is cool");
-console.log("Javascript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
