c20e96eadb20dc8f8e0735d8d768f5b0d8edbab2
lisa271 <lizbmaloba@gmail.com>
2023-04-13 15:10:58 -0700

    multi languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-//a script printing three lines.
+//a script that prints three lines
 console.log('C is fun');
 console.log('Python is cool');
-console.log('Javascript is amazing');
+console.log('JavaScript is amazing');
